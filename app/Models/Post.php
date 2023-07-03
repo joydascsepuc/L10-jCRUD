fb169a9a8ea5194e601b66654abca57eb4c67964
Joy Das <joydascsepuc@gmail.com>
2023-07-03 17:03:21 +0600

    Added relationship between models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\PostImage;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,4 +14,16 @@ class Post extends Model
     protected $table = 'posts';
 
     protected $guarded = ['id']; // For mass assignment variable
+
+    // Belongs to user
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id', 'user_id');
+    }
+
+    // Get post images
+    public function images()
+    {
+        return $this->hasMany(PostImage::class, 'post_id', 'id');
+    }
 }
